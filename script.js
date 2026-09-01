fc0a045b9e03e7529f93f691e89b37da8f801954
Fabio Gabrielli <macbookpro@MacBookProGabrielli.local>
2026-09-01 17:03:45 -0300

    js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ if (toggle && mobileMenu) {
   }));
 }
 
-const observer = new IntersectionObserver((entries) => {
+const observer = new IntersectionObserver((entries) => { 
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       entry.target.classList.add('visible');
